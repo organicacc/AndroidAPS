eb8c4ea3aced74f53cb1fdd5b0eaa7d737358bea
organicacc <abhishekrockzz007@gmail.com>
2026-04-07 11:48:19 +0530

    Update Objective3.kt
    Reduced objective4 time (in code it is objective 3)

--- plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/objectives/Objective3.kt
+++ plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/objectives/Objective3.kt
@@ -18,7 +18,7 @@ class Objective3 @Inject constructor(
 ) : Objective(preferences, rh, dateUtil, "openloop", R.string.objectives_openloop_objective, R.string.objectives_openloop_gate) {
 
     init {
-        tasks.add(MinimumDurationTask(this, T.days(7).msecs()))
+        tasks.add(MinimumDurationTask(this, T.mins(7).msecs()))
         tasks.add(
             object : Task(this, R.string.objectives_manualenacts) {
                 override fun isCompleted(): Boolean {
